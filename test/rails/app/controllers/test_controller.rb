20d322e5d66d7ee2d8950bcb1e92e0844e320d14
sd <sd@71bf3dff-0d17-0410-8985-d5c41cf5d41c>
2007-02-26 15:25:14 +0000

    Simplify test app
    git-svn-id: https://dev.notso.net/svn/rails/plugins/embedded_actions/trunk@83 71bf3dff-0d17-0410-8985-d5c41cf5d41c

--- test/rails/app/controllers/test_controller.rb
+++ test/rails/app/controllers/test_controller.rb
@@ -1,19 +1,19 @@
-class EmbeddedActionsTestController < ActionController::Base
+class TestController < ActionController::Base
   cattr_accessor :test_value
 
   caches_embedded :cached_action
   def cached_action
     @id = params[:id]
-    @value = EmbeddedActionsTestController.test_value || "N/A"
+    @value = TestController.test_value || "N/A"
     
-    render :template => "embedded_actions_test/value", :layout => false
+    render :template => "test/value", :layout => false
   end
   
   def regular_action
     @id = params[:id]
     @value = EmbeddedActionsTestController.test_value || "N/A"
     
-    render :template => "embedded_actions_test/value", :layout => false
+    render :template => "test/value", :layout => false
   end
   
   def page_with_embedded_actions
